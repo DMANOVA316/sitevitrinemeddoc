3ba78f025cf1798917957591f562de416a50ba4e
fadhel <fadhelubain@gmail.com>
2024-12-10 13:16:08 +0300

    feat:crud ambulance

--- src/App.tsx
+++ src/App.tsx
@@ -26,120 +26,130 @@ import { NumberProvider } from "./contexts/NumberContext";
 import { InfoMeddocProvider } from "./contexts/InfoMeddocContext";
 import SocialMediaIndex from "./pages/Dashboard/SocialMedia/SocialMediaIndex";
 import { SocialMediaProvider } from "./contexts/SocialMediaContext";
+import AmbulanceList from "./pages/Dashboard/AmbulanceList";
+import React from 'react';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
 
 const queryClient = new QueryClient();
 
 const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <InfoMeddocProvider>
-        <EditPagesProvider>
-          <ServiceProvider>
-            <NumberProvider>
-              <Toaster richColors />
-              <BrowserRouter>
-                <Routes>
-                  {/* Routes publiques */}
-                  <Route
-                    path="/"
-                    element={
-                      <PublicLayout>
-                        <Index />
-                      </PublicLayout>
-                    }
-                  />
-                  <Route
-                    path="/login"
-                    element={
-                      <PublicLayout>
-                        <Login />
-                      </PublicLayout>
-                    }
-                  />
-                  <Route
-                    path="/pharmacies"
-                    element={
-                      <PublicLayout>
-                        <Pharmacies />
-                      </PublicLayout>
-                    }
-                  />
+  <Provider store={store}>
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <InfoMeddocProvider>
+          <EditPagesProvider>
+            <ServiceProvider>
+              <NumberProvider>
+                <Toaster richColors />
+                <BrowserRouter>
+                  <Routes>
+                    {/* Routes publiques */}
+                    <Route
+                      path="/"
+                      element={
+                        <PublicLayout>
+                          <Index />
+                        </PublicLayout>
+                      }
+                    />
+                    <Route
+                      path="/login"
+                      element={
+                        <PublicLayout>
+                          <Login />
+                        </PublicLayout>
+                      }
+                    />
+                    <Route
+                      path="/pharmacies"
+                      element={
+                        <PublicLayout>
+                          <Pharmacies />
+                        </PublicLayout>
+                      }
+                    />
 
-                  {/* Routes des services */}
-                  <Route
-                    path="/services/community"
-                    element={
-                      <PublicLayout>
-                        <Community />
-                      </PublicLayout>
-                    }
-                  />
-                  <Route
-                    path="/services/consulting"
-                    element={
-                      <PublicLayout>
-                        <Consulting />
-                      </PublicLayout>
-                    }
-                  />
-                  <Route
-                    path="/services/digital"
-                    element={
-                      <PublicLayout>
-                        <Digital />
-                      </PublicLayout>
-                    }
-                  />
+                    {/* Routes des services */}
+                    <Route
+                      path="/services/community"
+                      element={
+                        <PublicLayout>
+                          <Community />
+                        </PublicLayout>
+                      }
+                    />
+                    <Route
+                      path="/services/consulting"
+                      element={
+                        <PublicLayout>
+                          <Consulting />
+                        </PublicLayout>
+                      }
+                    />
+                    <Route
+                      path="/services/digital"
+                      element={
+                        <PublicLayout>
+                          <Digital />
+                        </PublicLayout>
+                      }
+                    />
 
-                  {/* Routes protégées pour le tableau de bord */}
-                  <Route element={<PrivateRoute />}>
-                    <Route element={<DashboardLayout />}>
-                      <Route path="/dashboard" element={<DashboardIndex />} />
-                      <Route
-                        path="/dashboard/pharmacies"
-                        element={<DashboardPharmacies />}
-                      />
-                      {/* Edition de page */}
-                      <Route
-                        path="/dashboard/page-meddoc/"
-                        element={<EditPageIndex />}
-                      />
-                      <Route
-                        path="/dashboard/partenaires"
-                        element={<PartnerIndex />}
-                      >
+                    {/* Routes protégées pour le tableau de bord */}
+                    <Route element={<PrivateRoute />}>
+                      <Route element={<DashboardLayout />}>
+                        <Route path="/dashboard" element={<DashboardIndex />} />
                         <Route
-                          path="/dashboard/partenaires/list"
-                          element={<PartnerList />}
+                          path="/dashboard/pharmacies"
+                          element={<DashboardPharmacies />}
+                        />
+                        <Route
+                          path="/dashboard/ambulances"
+                          element={<AmbulanceList />}
+                        />
+                        {/* Edition de page */}
+                        <Route
+                          path="/dashboard/page-meddoc/"
+                          element={<EditPageIndex />}
+                        />
+                        <Route
+                          path="/dashboard/partenaires"
+                          element={<PartnerIndex />}
+                        >
+                          <Route
+                            path="/dashboard/partenaires/list"
+                            element={<PartnerList />}
+                          />
+                        </Route>
+
+                        <Route
+                          path="/dashboard/reseaux-sociaux"
+                          element={
+                            <SocialMediaProvider>
+                              <SocialMediaIndex />
+                            </SocialMediaProvider>
+                          }
+                        />
+                        <Route
+                          path="/dashboard/services"
+                          element={<Services />}
+                        />
+                        <Route
+                          path="/dashboard/contact-meddoc"
+                          element={<NumberList />}
                         />
                       </Route>
-
-                      <Route
-                        path="/dashboard/reseaux-sociaux"
-                        element={
-                          <SocialMediaProvider>
-                            <SocialMediaIndex />
-                          </SocialMediaProvider>
-                        }
-                      />
-                      <Route
-                        path="/dashboard/services"
-                        element={<Services />}
-                      />
-                      <Route
-                        path="/dashboard/contact-meddoc"
-                        element={<NumberList />}
-                      />
                     </Route>
-                  </Route>
-                </Routes>
-              </BrowserRouter>
-            </NumberProvider>
-          </ServiceProvider>
-        </EditPagesProvider>
-      </InfoMeddocProvider>
-    </TooltipProvider>
-  </QueryClientProvider>
+                  </Routes>
+                </BrowserRouter>
+              </NumberProvider>
+            </ServiceProvider>
+          </EditPagesProvider>
+        </InfoMeddocProvider>
+      </TooltipProvider>
+    </QueryClientProvider>
+  </Provider>
 );
 
 export default App;
